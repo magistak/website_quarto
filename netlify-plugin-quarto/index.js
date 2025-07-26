a7e5c5c38d334b84a4e424a4ec2f94e0ebc441cf
magistak <magistak@gmail.com>
2025-07-26 13:47:44 +0200

    Fix Netlify build: add package.json and proper plugin setup

--- netlify-plugin-quarto/index.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process')
-
-module.exports = {
-    onPreBuild: () => {
-        console.log("Installing Quarto...")
-        execSync('curl -fsSL https://quarto.org/download/latest/quarto-linux-amd64.deb -o quarto.deb && sudo dpkg -i quarto.deb')
-    }
-}
